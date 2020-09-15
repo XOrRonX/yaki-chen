9737ba9759a6a622ce8f644bcec12021dfdf2485
Or Ron <57252684+XOrRonX@users.noreply.github.com>
2020-09-15 12:19:07 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,428 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>יקי חן מומחה לרפואה סינית</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<?php
+				// define variables and set to empty values
+				$nameErr = $emailErr = $genderErr = $websiteErr = "";
+				$name = $email = $gender = $comment = $website = "";
+
+				if ($_SERVER["REQUEST_METHOD"] == "POST") {
+					if (empty($_POST["name"])) {
+						$nameErr = "Name is required";
+					} else {
+						$name = test_input($_POST["name"]);
+						// check if name only contains letters and whitespace
+						if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+						$nameErr = "Only letters and white space allowed";
+						}
+					}
+					
+					if (empty($_POST["email"])) {
+						$emailErr = "Email is required";
+					} else {
+						$email = test_input($_POST["email"]);
+						// check if e-mail address is well-formed
+						if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+						$emailErr = "Invalid email format";
+						}
+					}
+
+					if (empty($_POST["message"])) {
+						$comment = "";
+					} else {
+						$comment = test_input($_POST["message"]);
+					}
+
+				
+				}
+
+				function test_input($data) {
+					$data = trim($data);
+					$data = stripslashes($data);
+					$data = htmlspecialchars($data);
+					return $data;
+				}
+
+				$email = "or.ron7@gmail.com";
+				$subject = "מעוניין בטיפול ברפואה סינית";
+				$body = $comment;
+				$headers = $name;
+
+				mail($to_email, $subject, $body, $headers);			
+							
+		?>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<div class="logo">
+							<span class="icon fa-gem"></span>
+						</div>
+						<div class="content">
+							<div class="inner">
+								<h1>יקי חן</h1>
+								<p>פתרון  לבעיות בריאות מן השורש,  תהליך טיפולי תוך כדי הקשבה ויחס אישי למטופל<br />
+								לפרטים נוספים יש לפנות למספר הפלאפון 054-5866483</p>
+							</div>
+						</div>
+						<nav>
+							<ul>
+								<li><a href="#intro">אודות</a></li>
+								<li><a href="#work">שיטות טיפול</a></li>
+								<li><a href="#about">רפואה סינית</a></li>
+								<li><a href="#contact">יצירת קשר</a></li>
+								<!--<li><a href="#elements">Elements</a></li>-->
+							</ul>
+						</nav>
+					</header>
+
+				<!-- Main -->
+					<div id="main">
+
+						<!-- Intro -->
+							<article id="intro" style="text-align: right;">
+								<h2 class="major"  style="text-align: right;">אודות</h2>
+								<span class="image main"><img src="images/pic02.jpg" alt="" /></span>
+								<p>
+									יקי חן, מטפל מוסמך מטעם האגודה לרפואה סינית בישראל, בוגר מסלול לימודי רפואה סינית בארצות הברית.
+בטיפול, יקי חן משלב טכניקות עבודה שונות ובניהן: דיקור סיני, דיקור יפני, צמחים וטכניקות מגע (טווינה).
+
+								</p>
+							</article>
+
+						<!-- Work -->
+							<article id="work" style="text-align: right;">
+								<h2 class="major">שיטות טיפול</h2>
+								<span class="image main" ><img src="images/pic01.jpg" alt="" /></span>
+								<p>שיטות הטיפול המרכזיות של הרפואה הסינית הן דיקור סיני, תזונה, צמחי מרפא וטכניקות מגע. בעזרת השיטות האלו מצליחה הרפואה הסינית לרפא וגם למנוע בעיות רפואיות רבות בכל גיל, בקרב ילדים, מבוגרים וגם קשישים.
+									הרפואה הסינית מתייחסת אל מחלה כחוסר איזון בגוף ומטרת הטיפולים הם להחזיר לגוף את האיזון שאבד.<br> לפי הרפואה הסינית בגוף יש כשלוש מאות וחמישים נקודות המשפיעות על בריאות הגוף, לכל נקודה יש תפקיד שונה, וטיפולי הדיקור, שבהן מחדיר המטפל מחט דקה אל הנקודות המתאימות, מעוררים את הגוף לפעול, מחזקים ומאזנים אותו.									
+									צמחי המרפא פועלים על המערכות הפנימיות של הגוף.									
+									רפואת הצמחים קיימת מזה אלפי שנים. ברפואה הסינית השימוש בצמחי מרפא הוא דרך פורמולות, המשלבות מספר צמחים ביחד כדי ליצור סינרגיזם.השימוש בצמחים יעיל למגוון רחב של בעיות כמו: כאבי ראש, חוסר איזון בלחץ דם, בעיות עיכול, סוכרת ועוד.									
+									רפואה סינית מטפלת בשורש הבעיה ולא רק בסימפטומים שלה. מומחי הרפואה הסינית יחפשו את השורש והסיבה להופעת הסימפטומים, יטפלו בסיבה, יעזרו לריפוי מלא ושלם וימנעו את החזרה של המחלה ושל הסימפטומים שלה.</p>				<p>					
+									הרפואה הסינית מטפלת בהצלחה במגוון בעיות רפואיות, בכאבים כרונים, בכאבי גב ופרקים, בכאבי מחזור חודשי, במיגרנות, בבעיות  במערכת העיכול, ביתר לחץ דם, בכולסטרול גבוה ובבעיות כלליות כמו מתח וחרדה, קשיים בשינה, עייפות ועוד. לעתים הטיפול האלטרנטיבי יתבצע במקביל לטיפול השמרני ולעתים הוא יעמוד בפני עצמו.
+									הרפואה הסינית היא רפואה מונעת .עקרונות הרפואה גורסים שלא די לחכות עד שמחלה או הכאב יתקפו את הגוף, אלא חשוב להקדים רפואה למכה, לחיות אורח חיים נכון ובריא, לחזק את הגוף ולסייע לו להתגבר על המחלות עוד לפני שהן הופיעו ולעבור טיפולים תקופתיים שנועדו לשמר את הגוף.
+								</p>
+							</article>
+
+						<!-- About -->
+							<article id="about" style="text-align: right;">
+								<h2 class="major">רפואה סינית</h2>
+								<span class="image main"><img src="images/pic03.jpg" alt="" /></span>
+								<p>רפואה סינית היא שם כולל למגוון של שיטות טיפול שונות שבהן נעשה שימוש למעלה מאלפיים שנה בסין. בעשורים האחרונים גם הרפואה הקונבנציונאלית החלה להכיר בעקרונות החיים וביעילות הטיפולים של הרפואה הסינית והיא משלבת טיפולים רבים במוסדות הרפואה המערביים, בכל העולם וגם בישראל. התוצאות המתקבלות מהטיפולים מובילות לשיפור משמעותי, גם לטווח הארוך, הן בבריאות הגופנית והן בבריאות הנפשית.
+									יקי חן הוא מומחה בתחום הרפואה הסינית, הוא מטפל במגוון רחב של מחלות ותופעות ומצליח לתת מענה במקרים רבים שבהם הרפואה הקונבנציונאלית  מצליחה לעזור פחות  או שנותרה ללא מענה מתאים.</p>
+								<p>אם התנסיתם בעבר בטיפולים בתחום הרפואה הסינית אתם ודאי מכירים ומודעים ליתרונות הרבים של הרפואה הסינית, אם לא התנסיתם זה הזמן להזמין פגישה לאבחון וטיפול ולגלות את נפלאות הרפואה הסינית.
+									כדי ליהנות מהתוצאות טובות חשוב לבחור במטפל שמכיר את כל תחומי ההתמחות של הרפואה הסינית וצבר ידע רב וניסיון בתחום. רק מטפל מיומן ומנוסה ידע לבחור עבורכם את הטיפול המתאים ולבצע אותו בהצלחה.									
+									יקי חן הוא מטפל מוסמך מטעם האגודה לרפואה הסינית בישראל, הוא הצליח לעזור לאינספור מטופלים, הוא ידע לעזור גם לכם.</p>
+							</article>
+
+						<!-- Contact -->
+							<article id="contact">
+								<h2 class="major">יצירת קשר</h2>
+								<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+									<div class="fields">
+										<div class="field half">
+											<label for="name">שם מלא</label>
+											<input type="text" name="name" id="name" />
+										</div>
+										<div class="field half">
+											<label for="email">אימייל</label>
+											<input type="text" name="email" id="email" />
+										</div>
+										<div class="field">
+											<label for="message">ההודעה שלך</label>
+											<textarea name="message" id="message" rows="4" ></textarea>
+										</div>
+									</div>
+									<ul class="actions">
+										<li><input type="submit" value="שלח הודעה" class="primary" /></li>
+										<li><input type="reset" value="נקה" /></li>
+									</ul>
+								</form>
+								<ul class="icons">
+									<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+									<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+									<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+									<li><a href="#" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+								</ul>
+							</article>
+
+								
+
+						<!-- Elements -->
+							<article id="elements">
+								<h2 class="major">Elements</h2>
+
+								<section>
+									<h3 class="major">Text</h3>
+									<p>This is <b>bold</b> and this is <strong>strong</strong>. This is <i>italic</i> and this is <em>emphasized</em>.
+									This is <sup>superscript</sup> text and this is <sub>subscript</sub> text.
+									This is <u>underlined</u> and this is code: <code>for (;;) { ... }</code>. Finally, <a href="#">this is a link</a>.</p>
+									<hr />
+									<h2>Heading Level 2</h2>
+									<h3>Heading Level 3</h3>
+									<h4>Heading Level 4</h4>
+									<h5>Heading Level 5</h5>
+									<h6>Heading Level 6</h6>
+									<hr />
+									<h4>Blockquote</h4>
+									<blockquote>Fringilla nisl. Donec accumsan interdum nisi, quis tincidunt felis sagittis eget tempus euismod. Vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan faucibus. Vestibulum ante ipsum primis in faucibus lorem ipsum dolor sit amet nullam adipiscing eu felis.</blockquote>
+									<h4>Preformatted</h4>
+									<pre><code>i = 0;
+
+while (!deck.isInOrder()) {
+    print 'Iteration ' + i;
+    deck.shuffle();
+    i++;
+}
+
+print 'It took ' + i + ' iterations to sort the deck.';</code></pre>
+								</section>
+
+								<section>
+									<h3 class="major">Lists</h3>
+
+									<h4>Unordered</h4>
+									<ul>
+										<li>Dolor pulvinar etiam.</li>
+										<li>Sagittis adipiscing.</li>
+										<li>Felis enim feugiat.</li>
+									</ul>
+
+									<h4>Alternate</h4>
+									<ul class="alt">
+										<li>Dolor pulvinar etiam.</li>
+										<li>Sagittis adipiscing.</li>
+										<li>Felis enim feugiat.</li>
+									</ul>
+
+									<h4>Ordered</h4>
+									<ol>
+										<li>Dolor pulvinar etiam.</li>
+										<li>Etiam vel felis viverra.</li>
+										<li>Felis enim feugiat.</li>
+										<li>Dolor pulvinar etiam.</li>
+										<li>Etiam vel felis lorem.</li>
+										<li>Felis enim et feugiat.</li>
+									</ol>
+									<h4>Icons</h4>
+									<ul class="icons">
+										<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+										<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+										<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+										<li><a href="#" class="icon brands fa-github"><span class="label">Github</span></a></li>
+									</ul>
+
+									<h4>Actions</h4>
+									<ul class="actions">
+										<li><a href="#" class="button primary">Default</a></li>
+										<li><a href="#" class="button">Default</a></li>
+									</ul>
+									<ul class="actions stacked">
+										<li><a href="#" class="button primary">Default</a></li>
+										<li><a href="#" class="button">Default</a></li>
+									</ul>
+								</section>
+
+								<section>
+									<h3 class="major">Table</h3>
+									<h4>Default</h4>
+									<div class="table-wrapper">
+										<table>
+											<thead>
+												<tr>
+													<th>Name</th>
+													<th>Description</th>
+													<th>Price</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>Item One</td>
+													<td>Ante turpis integer aliquet porttitor.</td>
+													<td>29.99</td>
+												</tr>
+												<tr>
+													<td>Item Two</td>
+													<td>Vis ac commodo adipiscing arcu aliquet.</td>
+													<td>19.99</td>
+												</tr>
+												<tr>
+													<td>Item Three</td>
+													<td> Morbi faucibus arcu accumsan lorem.</td>
+													<td>29.99</td>
+												</tr>
+												<tr>
+													<td>Item Four</td>
+													<td>Vitae integer tempus condimentum.</td>
+													<td>19.99</td>
+												</tr>
+												<tr>
+													<td>Item Five</td>
+													<td>Ante turpis integer aliquet porttitor.</td>
+													<td>29.99</td>
+												</tr>
+											</tbody>
+											<tfoot>
+												<tr>
+													<td colspan="2"></td>
+													<td>100.00</td>
+												</tr>
+											</tfoot>
+										</table>
+									</div>
+
+									<h4>Alternate</h4>
+									<div class="table-wrapper">
+										<table class="alt">
+											<thead>
+												<tr>
+													<th>Name</th>
+													<th>Description</th>
+													<th>Price</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>Item One</td>
+													<td>Ante turpis integer aliquet porttitor.</td>
+													<td>29.99</td>
+												</tr>
+												<tr>
+													<td>Item Two</td>
+													<td>Vis ac commodo adipiscing arcu aliquet.</td>
+													<td>19.99</td>
+												</tr>
+												<tr>
+													<td>Item Three</td>
+													<td> Morbi faucibus arcu accumsan lorem.</td>
+													<td>29.99</td>
+												</tr>
+												<tr>
+													<td>Item Four</td>
+													<td>Vitae integer tempus condimentum.</td>
+													<td>19.99</td>
+												</tr>
+												<tr>
+													<td>Item Five</td>
+													<td>Ante turpis integer aliquet porttitor.</td>
+													<td>29.99</td>
+												</tr>
+											</tbody>
+											<tfoot>
+												<tr>
+													<td colspan="2"></td>
+													<td>100.00</td>
+												</tr>
+											</tfoot>
+										</table>
+									</div>
+								</section>
+
+								<section>
+									<h3 class="major">Buttons</h3>
+									<ul class="actions">
+										<li><a href="#" class="button primary">Primary</a></li>
+										<li><a href="#" class="button">Default</a></li>
+									</ul>
+									<ul class="actions">
+										<li><a href="#" class="button">Default</a></li>
+										<li><a href="#" class="button small">Small</a></li>
+									</ul>
+									<ul class="actions">
+										<li><a href="#" class="button primary icon solid fa-download">Icon</a></li>
+										<li><a href="#" class="button icon solid fa-download">Icon</a></li>
+									</ul>
+									<ul class="actions">
+										<li><span class="button primary disabled">Disabled</span></li>
+										<li><span class="button disabled">Disabled</span></li>
+									</ul>
+								</section>
+
+								<section>
+									<h3 class="major">Form</h3>
+									<form method="post" action="#">
+										<div class="fields">
+											<div class="field half">
+												<label for="demo-name">Name</label>
+												<input type="text" name="demo-name" id="demo-name" value="" placeholder="Jane Doe" />
+											</div>
+											<div class="field half">
+												<label for="demo-email">Email</label>
+												<input type="email" name="demo-email" id="demo-email" value="" placeholder="jane@untitled.tld" />
+											</div>
+											<div class="field">
+												<label for="demo-category">Category</label>
+												<select name="demo-category" id="demo-category">
+													<option value="">-</option>
+													<option value="1">Manufacturing</option>
+													<option value="1">Shipping</option>
+													<option value="1">Administration</option>
+													<option value="1">Human Resources</option>
+												</select>
+											</div>
+											<div class="field half">
+												<input type="radio" id="demo-priority-low" name="demo-priority" checked>
+												<label for="demo-priority-low">Low</label>
+											</div>
+											<div class="field half">
+												<input type="radio" id="demo-priority-high" name="demo-priority">
+												<label for="demo-priority-high">High</label>
+											</div>
+											<div class="field half">
+												<input type="checkbox" id="demo-copy" name="demo-copy">
+												<label for="demo-copy">Email me a copy</label>
+											</div>
+											<div class="field half">
+												<input type="checkbox" id="demo-human" name="demo-human" checked>
+												<label for="demo-human">Not a robot</label>
+											</div>
+											<div class="field">
+												<label for="demo-message">Message</label>
+												<textarea name="demo-message" id="demo-message" placeholder="Enter your message" rows="6"></textarea>
+											</div>
+										</div>
+										<ul class="actions">
+											<li><input type="submit" value="Send Message" class="primary" /></li>
+											<li><input type="reset" value="Reset" /></li>
+										</ul>
+									</form>
+								</section>
+
+							</article>
+
+					</div>
+
+				
+					<footer id="footer">
+						<p class="copyright">יקי חן מומחה לרפואה סינית</a></p>
+					</footer>
+
+			</div>
+
+		<!-- BG -->
+			<div id="bg"></div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+
+			
+		
+			
+
+	</body>
+</html>
