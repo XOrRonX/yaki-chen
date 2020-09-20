ea13047dbdb7d2f096ced7cbe86a6bcae8f3e20e
Or Ron <or.ron7@gmail.com>
2020-09-20 21:49:55 +0300

    ד

--- index.php
+++ index.php
@@ -77,8 +77,8 @@
 							</article>
 
 						<!-- Contact -->
-							<article id="contact" style="text-align: right; direction: rtl;">
-								<h2 class="major" >יצירת קשר וקביעת פגישה</h2>
+							<article id="contact" >
+								<h2 class="major" >יצירת קשר וקביעת פגישת ייעוץ</h2>
 								<ul class="icons" style="text-align: center;">
 								<li><a class="icon brands fa-whatsapp"  href="https://wa.me/+972545866483?text=Hi%20Yaki%20I%20would%20like%20to%20arrange%20an%20appointment%20to..."></a></li>
 								<li><a class="icon solid fa-envelope" href="mailto:omrychen15793@gmail.com?subject=Hi%20Yaki%20&body=Hi%20Yaki%20I%20would%20like%20to%20arrange%20an%20appointment%20to..." rel="EMAIL"></a></li>							
